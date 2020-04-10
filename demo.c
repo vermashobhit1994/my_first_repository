3f22a0c36e4d3c655c547aa659831c936eecc282
shobhit <vermashobhit1994@gmail.com>
2020-04-10 19:23:00 +0530

    edited AUTHOR file and added demo.c

---
+++ demo.c
@@ -0,0 +1,40 @@
+/*#include<stdio.h> 
+
+// Value of 'a' can be accessed/modified either 
+// through 'a' or through 'b' 
+int  a = 5; 
+int *b = &a; 
+
+int func( double* restrict b) 
+{ 
+	a = 1; 
+	printf("%p %p %p %d\n",b,&b,&a,a);
+	// The below statement modifies 'a' 
+	*b = 1234.56;
+	//printf("%p %d\n",b,*c);
+	//printf("%d\n",a);
+	return (a); 
+} 
+
+int main() 
+{ 
+	printf("%d\n",a);
+	printf("%d\n", func((double*)&a)); 
+
+	printf("%d",a);
+	return 0; 
+} 
+*/
+
+
+#include <string.h>
+
+  #include <stdio.h>
+
+   int main(void) {
+
+     char * p = (char*)malloc(10);
+
+     strcpy(p, "trouble");
+
+     printf("%s\n", p);}
